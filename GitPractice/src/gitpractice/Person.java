6d2f785a2203f8dfc7cf2c8953d2b73b44d6207e
Pálmai Mercédesz <palmaim@hotmail.com>
2019-05-17 14:15:38 +0200

    PersonClassnewfields

--- GitPractice/src/gitpractice/Person.java
+++ GitPractice/src/gitpractice/Person.java
@@ -10,27 +10,84 @@ package gitpractice;
  * @author progmatic
  */
 public class Person {
-    private String name;
-    private String email;
+    private String firstName;
+    private String lastName;
+    private String companyName;
+    private String address;
     private String city;
+    private String county;
+    private String state;
+    private int zip;
+    private String phone1;
+    private String phone2;
+    private String email;
+    private String web;
 
-    public Person(String name, String email, String city) {
-        this.name = name;
-        this.email = email;
+    public Person(String firstName, String lastName, String companyName, String address, String city, String county, String state, int zip, String phone1, String phone2, String email, String web) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.companyName = companyName;
+        this.address = address;
         this.city = city;
+        this.county = county;
+        this.state = state;
+        this.zip = zip;
+        this.phone1 = phone1;
+        this.phone2 = phone2;
+        this.email = email;
+        this.web = web;
     }
 
-    public String getName() {
-        return name;
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getCounty() {
+        return county;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public String getPhone1() {
+        return phone1;
+    }
+
+    public String getPhone2() {
+        return phone2;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public String getCity() {
-        return city;
+    public String getWeb() {
+        return web;
     }
+    
+
+    
     
     
     
